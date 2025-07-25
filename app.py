507cf69ea64532799ffa3461d1f84c610e7f49c9
abnas <abnasgit@gmail.com>
2025-07-25 14:04:06 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', year=datetime.now().year)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', year=datetime.now().year)
+
+@app.route('/services')
+def services():
+    return render_template('services.html', year=datetime.now().year)
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', year=datetime.now().year)
+
+if __name__ == '__main__':
+    app.run(debug=True)
